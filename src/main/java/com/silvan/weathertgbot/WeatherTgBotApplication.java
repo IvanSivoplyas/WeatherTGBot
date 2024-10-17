46959751c7375de7bfd2eec398955b9d6a3eaa02
Ivan Sivoplyas <124461656+IvanSivoplyas@users.noreply.github.com>
2024-10-17 22:48:41 +0400

    Add files via upload

---
+++ src/main/java/com/silvan/weathertgbot/WeatherTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.silvan.weathertgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silvan/weathertgbot/WeatherTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvan.weathertgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
